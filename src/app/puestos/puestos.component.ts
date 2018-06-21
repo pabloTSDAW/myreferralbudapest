01ed7226ce32f94ccacd477f3db27413b4f245fd
Pablo Prieto <pablotsdaw@gmail.com>
2018-06-21 13:18:16 +0200

    puestos con scroll infinito

--- src/app/puestos/puestos.component.ts
+++ src/app/puestos/puestos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../firebase.service';
 
 @Component({
   selector: 'app-puestos',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PuestosComponent implements OnInit {
 
-  constructor() { }
+  puestos;
+  puestosGeneral;
+  cont: number = 4;
+  comprobar: boolean = false;
+
+  constructor(private firebase: FirebaseService) { }
 
   ngOnInit() {
+    this.firebase.empresa$.subscribe(data => {
+      this.empresaNombre = data.nombre;
+    });
+
+    this.peticionOfertas(this.cont);
+
+    //SCROLL INFINITO
+      var win = $(window);
+
+      // Cada vez que el usuario hace scroll
+      var self = this;
+      win.scroll(function() {
+
+        if (($(document).height() - win.height() <= (win.scrollTop()) + 20) && ($(document).height() - win.height() >= (win.scrollTop()) - 20)) {
+          self.comprobar = true;
+          if (self.comprobar == true) {
+            self.cont++;
+            self.peticionOfertas(self.cont);
+            self.comprobar = false;
+          }
+        }
+      });
+  }
+
+  //Recoge todas las ofertas de la base de datos
+  peticionOfertas(cont) {
+    this.firebase.getPuestos().subscribe(data => {
+      this.empresa = data;
+      this.puestosGeneral = [];
+      for (let puesto of this.empresa) {
+        this.puestosGeneral.push(puesto);
+      };
+      this.puestos = this.puestosGeneral.splice(0, cont);
+    });
+  }
+
+  //Elige una oferta y la pasa como parámetro a la vista individual
+  seleccionarOferta(elem) {
+    this.firebase.setOferta(elem);
   }
 
 }
